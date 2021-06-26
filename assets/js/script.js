5c6b9ec5adde3fa6e7288c7032ef2d20f03719f3
Vladislav <64088661+alteropt@users.noreply.github.com>
2021-06-26 13:54:52 +0300

    Add scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,33 @@
-$(window).on('scroll', function() {
-	$('header').addClass('active');
-	if ($(this).scrollTop() == 0) {
-		$('header').removeClass('active');
+$(function() {
+	$(window).on('scroll', function() {
+		$('header').addClass('active');
+		if ($(this).scrollTop() == 0) {
+			$('header').removeClass('active');
+		}
+	});
+	if ($('header').offset().top > 0) {
+		$('.header').toggleClass('active');
 	}
-});
 
+	
+	$('.header__burger-menu').on('click', function() {
+		$('.header__burger-menu').toggleClass('active');
+		$('.header__navigation').toggleClass('active');
+	});
 
-$('.header__burger-menu').on('click', function() {
-	$('.header__burger-menu').toggleClass('active');
-	$('.header__navigation').toggleClass('active');
+	$('[data-scroll]').on('click', function(event) {
+		$('[data-scroll]').removeClass('active');
+		var blockSelector = $(this).data('scroll');
+		var blockOffset = $(blockSelector).offset().top;
+		var headerOffset = $('header').innerHeight();
+
+		$('html, body').animate({
+			scrollTop: blockOffset - headerOffset,
+		}, 500);
+
+		$(this).addClass('active');
+	});
+
 });
 
 
